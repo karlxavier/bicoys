c4d9781d464fb59ac4281f6746cb736b60399c5b
karlxavier <kxdc1025@yahoo.com>
2017-02-04 15:25:07 +0400

    added some style on user login and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  # devise_for :users
 	root 'restaurant_types#index'
+	get 'admin' => 'admin#my_dashboard'
+
+	devise_for :users, controllers: {
+        sessions: 'users/sessions'
+    }
 
 	resources :restaurants, only: [:index, :show] do
 		resources :menus, only: [:index, :show]
