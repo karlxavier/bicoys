e41c712b597219ab9e8e02644c86d8c03672358b
karlxavier <kxdc1025@yahoo.com>
2017-12-09 13:20:27 +0400

    app almost ready for production

--- app/controllers/restos/restaurants_controller.rb
+++ app/controllers/restos/restaurants_controller.rb
@@ -1,14 +1,20 @@
 class Restos::RestaurantsController < ApplicationController
 	before_action :authenticate_resto!
-	# before_action :set_restaurant, only: [:edit, :update, :show]
+	before_action :set_restaurant
 
 	def index
 	end
 
 	def restodb
-		@restaurant = Restaurant.find(current_resto.restaurant_id)
-		@resto_stars = Restaurant.resto_stars(@restaurant.id)
+		@restaurant = Restaurant.find(@restaurant.id)
+		@resto_stars = Rate.resto_stars(@restaurant.id)
 		@voters_count = Rate.where(rateable_type: "Restaurant", rateable_id: @restaurant.id).count
 	end
+
+	private
+
+		def set_restaurant
+			@restaurant = Restaurant.find(current_resto.restaurant_id)
+		end
 
 end
